4c4dd18e1944a1c2290eda65ac086d3244290b6d
Stephen Howe <mrstephenhowe@gmail.com>
2026-04-08 18:20:05 +0200

    Relative path resolution for connecting to db during analysis.

--- src/festival_analysis/analysis_utils.py
+++ src/festival_analysis/analysis_utils.py
@@ -1,7 +1,9 @@
 import duckdb
 import pandas as pd
 
+from .config import DB_PATH
+
 
 def query_db(query: str) -> pd.DataFrame:
-    con = duckdb.connect("./data/festival.duckdb", read_only=True)
+    con = duckdb.connect(str(DB_PATH), read_only=True)
     return con.execute(query=query).df()
