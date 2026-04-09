61b635f3ef7a36a2e42219842710ddd102a07e3d
Stephen Howe <mrstephenhowe@gmail.com>
2026-04-09 11:28:22 +0200

    Adding `index` param to `query_db` to preload df with index

--- src/festival_analysis/analysis_utils.py
+++ src/festival_analysis/analysis_utils.py
@@ -4,6 +4,11 @@ import pandas as pd
 from .config import DB_PATH
 
 
-def query_db(query: str) -> pd.DataFrame:
+def query_db(query: str, index: str | None = None) -> pd.DataFrame:
     con = duckdb.connect(str(DB_PATH), read_only=True)
-    return con.execute(query=query).df()
+    df = con.execute(query=query).df()
+
+    if index is not None:
+        df.set_index(index, inplace=True)
+
+    return df
